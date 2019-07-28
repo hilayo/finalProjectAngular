e8fab33fd56c44f00095f9443006b707d3d7505a
unknown <hilalosh10@gmail.com>
2019-07-28 15:34:47 +0300

    add Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule, Injectable } from '@angular/core';
-import { Routes, RouterModule, CanActivate } from '@angular/router';
+import { Routes, RouterModule, CanActivate, Router } from '@angular/router';
 import { LoginComponent } from './module-login/login/login.component';
 import { signUpComponent } from './module-login/signUp/signUp.component';
-//import { SLoginService } from './module-login/slogin.service';
+import { SLoginService } from './module-login/slogin.service';
 
 // @Injectable()
 //  class OnlyLoggedInUsersGuard implements CanActivate {
@@ -12,14 +12,26 @@ import { signUpComponent } from './module-login/signUp/signUp.component';
 //   }
 // }
 
-
+@Injectable({
+  providedIn: 'root',
+})
+export class OnlyLoggedInUsersGuard implements CanActivate{
+  constructor(private slogin:SLoginService , private router: Router) {}
+  canActivate(): boolean {
+    console.log('AuthGuard#canActivate called');
+    if(!!this.slogin.getUserId())
+           return true;
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
 const routes: Routes = [
 
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'homePage',
     loadChildren: './homepage/home-page.module#HomePageModule',
-  //  canActivate: [OnlyLoggedInUsersGuard]
+    canActivate: [OnlyLoggedInUsersGuard]
   },
   { path: 'login', component: LoginComponent },
   { path: 'signUp', component: signUpComponent },
