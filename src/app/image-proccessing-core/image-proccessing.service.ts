fa6ca8416b552f0d7767c7affeda9a2d0edfe70d
unknown <hilalosh10@gmail.com>
2019-07-30 02:53:59 +0300

    marge

--- src/app/image-proccessing-core/image-proccessing.service.ts
+++ src/app/image-proccessing-core/image-proccessing.service.ts
@@ -17,24 +17,76 @@ export class ImageProccessingService {
 
 
   CallImageProccessingApi(ImageBase64: string): Observable<any> {
+
+   // var img = new Buffer(ImageBase64, 'base64');
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/octet-stream',
+       //'Content-Type': 'image/jpeg',
+      // 'Content-Type': 'multipart/form-data',
+       //image/jpeg
+       // 'Content-Length': img.length,
         'Ocp-Apim-Subscription-Key': this.apiKey,
         'Access-Control-Allow-Origin': "*"
       })
     };
-    const body = { "url": "https://ae01.alicdn.com/kf/HTB1GUs2JpXXXXakXXXXq6xXFXXXz/2015-fall-sweaters-for-women-pull-long-sweater-woman-pullovers-knitwear-fashion-mori-girl-sueter-mujer.jpg" }
+   // const contentType = 'image/png';
+   var d:any=this.dataURItoBlob(ImageBase64);
+  //  const blob = this.dataURItoBlob(ImageBase64);
+    // const blobUrl = URL.createObjectURL(blob);
+        const body = { "url": d };
+   // const body = { "url": "https://ae01.alicdn.com/kf/HTB1GUs2JpXXXXakXXXXq6xXFXXXz/2015-fall-sweaters-for-women-pull-long-sweater-woman-pullovers-knitwear-fashion-mori-girl-sueter-mujer.jpg" }
     return this.http.post<imageProccessingOutput>(this.url, body, httpOptions).pipe(map(result =>
       new imageProccessingMinOutput(result.color, result.tags, result.description)
-    ));
-    // const body = { "url": ImageBase64 };
-    // return this.http.post<imageProccessingOutput>(this.url, body, httpOptions).pipe(map(result =>
-    //   new imageProccessingMinOutput(result.color, result.tags, result.description)
-    // ));
+   ));
+
+
+    //const body = { "url": img };
 
 
   }
+  CallImageProccessingApiBase64(ImageBase64: string): Observable<any> {
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'image/jpeg',
+       // 'Content-Length': img.length,
+        'Ocp-Apim-Subscription-Key': this.apiKey,
+        'Access-Control-Allow-Origin': "*"
+      })
+    };
+    //link
+    const body  = { "url": ImageBase64 };
+       //const body = { "url": d };
+    return this.http.post<imageProccessingOutput>(this.url, body, httpOptions).pipe(map(result =>
+      new imageProccessingMinOutput(result.color, result.tags, result.description)
+   ));
+
+  }
+
+
+ dataURItoBlob(dataURI) {
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  var byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0)
+      byteString = atob(dataURI.split(',')[1]);
+  else
+      byteString = unescape(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  // write the bytes of the string to a typed array
+  var ia = new Uint8Array(byteString.length);
+  for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ia], {type:mimeString});
+}
+
+
+
 }
 
 
